ac9cfaeea4d1fc74a67a8e6b14f1e8163cc5988b
Harishkumarharry <54526806+Harishkumarharry@users.noreply.github.com>
2021-05-20 20:42:06 +0530

    Script
    Add script to a component so that it will activate and execute.

--- 1_Introduction/Assets/IntroductionToCSharp.cs
+++ 1_Introduction/Assets/IntroductionToCSharp.cs
@@ -7,7 +7,7 @@ public class IntroductionToCSharp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hi Harish");
+        Debug.Log("Printing in console from script");
     }
 
     // Update is called once per frame
